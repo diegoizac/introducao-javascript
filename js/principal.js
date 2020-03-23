53c5fed5aded33e1a5cfe8be2ee018cefb695f53
Diego Izac Luis da Silva <diegoizac@hotmail.com>
2020-03-23 17:46:09 -0300

    Adição dos arquivos do download 3

--- js/principal.js
+++ js/principal.js
@@ -1,7 +1,4 @@
-console.log("Fui carregado de um arquivo externo");
-
 var titulo = document.querySelector(".titulo");
-
 titulo.textContent = "Aparecida Nutricionista";
 
 var paciente = document.querySelector("#primeiro-paciente");
@@ -17,27 +14,19 @@ var tdImc = paciente.querySelector(".info-imc");
 var pesoEhValido = true;
 var alturaEhValida = true;
 
-if(peso <= 0 || peso >= 1000){
-  console.log("Peso inválido!");
-  pesoEhValido = false;
-  tdImc.textContent = "Peso inválido!"
+if (peso <= 0 || peso >= 1000) {
+    console.log("Peso inválido!");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido";
 }
 
-if(altura <= 0 || altura >= 3.00){
-  console.log("Altura inválida!");
-  alturaEhValida = false;
-  tdImc.textContent = "Altura inválida!"
+if (altura <= 0 || altura >= 3.00) {
+    console.log("Altura inválida!");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida";
 }
 
-if (alturaEhValida && pesoEhValido){
-
-  var imc = peso / (altura * altura);
-  tdImc.textContent = imc;
-} else {
-  tdImc.textContent = "Altura e/ou peso inválidos!"
+if (pesoEhValido && alturaEhValida) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
 }
-
-
-
-
-console.log(imc);
