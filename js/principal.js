4e5d7c9fe7e89abf8ec2ffe284086bb798c94778
Diego Izac Luis da Silva <diego.silva@pampa.compasso>
2020-03-23 11:27:11 -0300

    Curso de introdução ao Java Script Alura

---
+++ js/principal.js
@@ -0,0 +1,25 @@
+console.log("Fui carregado de um arquivo externo");
+
+var titulo = document.querySelector(".titulo");
+
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+if(peso < 0 || peso > 1000){
+  console.log("Peso inválido!");
+}
+
+var imc = peso / (altura * altura); // 100 / (2.0 * 2.0) = 100 / 4 =>>>>>> 25
+
+tdImc.textContent = imc;
+
+console.log(imc);
