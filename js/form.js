14590e55808ec91a8ceba0de5f5f8477c8725ee4
Diego Izac Luis da Silva <diegoizac@hotmail.com>
2020-03-26 09:00:58 -0300

    Comite de atualização da base de arquivos

--- js/form.js
+++ js/form.js
@@ -1,14 +1,12 @@
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function(event) {
+botaoAdicionar.addEventListener("click", function (event) {
     event.preventDefault();
 
     var form = document.querySelector("#form-adiciona");
+    // Extraindo informações do paciente do form
+    obtemPacienteDoFormulario(form);
 
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
-
+    // cria a tr a td do paciente
     var pacienteTr = document.createElement("tr");
 
     var nomeTd = document.createElement("td");
@@ -21,14 +19,26 @@ botaoAdicionar.addEventListener("click", function(event) {
     pesoTd.textContent = peso;
     alturaTd.textContent = altura;
     gorduraTd.textContent = gordura;
+    imcTd.textContent = calculaImc(peso, altura);
 
     pacienteTr.appendChild(nomeTd);
     pacienteTr.appendChild(pesoTd);
     pacienteTr.appendChild(alturaTd);
     pacienteTr.appendChild(gorduraTd);
-    
+    pacienteTr.appendChild(imcTd);
 
+    // adicionando o paciente na tabela
     var tabela = document.querySelector("#tabela-pacientes");
 
     tabela.appendChild(pacienteTr);
 });
+
+function obtemPacienteDoFormulario(form) {
+
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value
+    }
+}
